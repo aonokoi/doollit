04bb4bc2a0c98c625af934497da0326c4d0091ac
aonokoi <nazhimov2005@mail.ru>
2026-05-09 22:56:52 +0500

    extend conf_env_local

--- utils/conf_env_local.sh
+++ utils/conf_env_local.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-
+# Paste it to .bashrc
+#
+# DB config
 export DB_USER=postgres
 export DB_PASSWORD=admin
 export DB_HOST=localhost
 export DB_PORT=5432
 export DB_NAME=gotodo
+
+# HTTP config
+export HTTP_PORT=8083
+export HTTP_HOST=localhost
